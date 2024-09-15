9bd96d856347225e1ab44d8ed765866aa9a25614
Panagiotis Liampas <pliam1105@gmail.com>
2024-09-15 01:56:27 -0400

    ACTUALLY PUSH THE WORKING CODE THIS TIME

--- Raspberry Pi/data_receive_transmit.py
+++ Raspberry Pi/data_receive_transmit.py
@@ -1,13 +1,15 @@
 import serial
 import io
+import time
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
-lat = 0
-lon = 0
+lat = 42.357978
+lon = -71.095933
+pulse_rate = 70
 # ser = serial.serial_for_url('/dev/ttyUSB0', 115200, timeout=1)
 # sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-def connect_murata():
+def connect_murata(satellite = False):
     global ser, sio
     while(not ser.is_open):
         ser.open()
@@ -24,8 +26,10 @@ def connect_murata():
     ensure_send_command('AT%SETACFG="locsrv.operation.locsrv_enable","true"')
     ensure_send_command('AT%SETACFG="locsrv.internal_gnss.auto_restart","enable"')
     ensure_send_command('AT%SETACFG="modem_apps.Mode.AutoConnectMode","true"')
-    # ensure_send_command('AT%RATACT="NBNTN","1"') # if I want satellite
-    ensure_send_command('AT%RATACT="CATM","1"') # if I want AT&T
+    if(satellite):
+        ensure_send_command('AT%RATACT="NBNTN","1"') # if I want satellite
+    else:
+        ensure_send_command('AT%RATACT="CATM","1"') # if I want AT&T
     ensure_send_command('AT+CFUN=0') # stop radio
     ensure_send_command('AT%PDNSET=1,"DATA.MONO","IP"')
     ensure_send_command('AT%SETSYSCFG=SW_CFG.nb_band_table.band#1,ENABLE;23')
@@ -33,25 +37,32 @@ def connect_murata():
     ensure_send_command('ATZ', '%BOOTEV:0') # restart
     ensure_send_command('AT+CFUN=0') # only need to do again if I restart the module
     ensure_send_command('AT%IGNSSEV="FIX",1') # GPS coordinates subscription
-    # ensure_send_command('AT%NOTIFYEV="SIB31",10') # satellite subscription
+    if(satellite):
+        ensure_send_command('AT%NOTIFYEV="SIB31",1') # satellite subscription
     ensure_send_command('AT+CEREG=2')
-    ensure_send_command('AT%IGNSSACT=1')
+    if(satellite):
+        ensure_send_command('AT%IGNSSACT=1', '%IGNSSEVU')
     ensure_send_command('AT+CFUN=1', '+CEREG: 5') # reenable radio
-    ensure_send_command('AT+COPS?')
+    # ensure_send_command('AT+COPS?')
 
 def ensure_send_command(command, confirm = "OK"):
     ret = ""
-    while(len(ret) == 0 or ret[0] != command):
+    while(len(ret) == 0 or ("ERROR" in ret)):
         ret = send_command(command)
+        # if(check_gps):
+        #     parse_gps(ret)
     while(len(ret)==0 or ret[-1][:len(confirm)] != confirm):
-        ret = read_response()
-        parse_gps(ret)
+        ret = pull_responses()
+        # if(check_gps):
+        #     parse_gps(ret)
             
 def parse_gps(ret):
     global lat,lon
     it = 0
     while(it < len(ret)-1 and (len(ret[it]) < len("%IGNSSEVU") or ret[it][:len("%IGNSSEVU")] != "%IGNSSEVU")):
         it += 1
+    if(len(ret) == 0):
+        return
     if ret[it][:len("%IGNSSEVU")] == "%IGNSSEVU":
         chopped = ret[it].split(",")
         lat = float(chopped[4].replace('"',''))
@@ -63,9 +74,17 @@ def send_command(command):
     # sio.flush()
     ser.write(bytes(command + '\r\n','utf-8'))
     ser.flush()
-    return read_response()
+    return pull_responses()
 
-def read_response():
+def get_gps_send_packet(packet):
+    parse_gps(pull_responses())
+    ensure_send_command('AT%SOCKETCMD="ALLOCATE",1,"UDP","OPEN","172.104.219.246",5000,0')
+    ensure_send_command('AT%SOCKETCMD="SETOPT",1,36000,1')
+    ensure_send_command('AT%SOCKETCMD="ACTIVATE",1')
+    ensure_send_command('AT%SOCKETDATA="SEND",1,'+str(len(packet))+',"'+packet.encode().hex()+'"')
+    ensure_send_command('AT%SOCKETCMD="DELETE",1')
+
+def pull_responses():
     global ser, sio
     # print(sio.readline())
     ret = [r.decode().rstrip() for r in ser.readlines()]
@@ -78,12 +97,11 @@ def disconnect_murata():
 
 def standby():
     while True:
-        read_response()
+        pull_responses()
 
-parse_gps(['', '%IGNSSEVU: "FIX",1,"04:02:56","15/09/2024","42.357978","-71.095933","39.7",1726372976000,,,"B",3'])
-print(lat, lon)
-connect_murata()
-standby()
-# while(True):
-#     c = input("Enter command:")
-#     send_command(c)
+connect_murata(satellite=False)
+while(True):
+    # Record Arduino pulse rate data via Serial
+    
+    get_gps_send_packet('{"lat": '+str(lat)+', "lon": '+str(lon)+', "rate": '+str(pulse_rate)+', "id": 5}')
+    time.sleep(2)
