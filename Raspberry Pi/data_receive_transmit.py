8647914e5180b19fd5d8a4b2169de13344b4932d
Panagiotis Liampas <pliam1105@gmail.com>
2024-09-15 00:11:21 -0400

    IT FUCKING WORKS!!!

--- Raspberry Pi/data_receive_transmit.py
+++ Raspberry Pi/data_receive_transmit.py
@@ -10,40 +10,39 @@ def connect_murata():
     while(not ser.is_open):
         ser.open()
     print("PORT OPEN")
+    ensure_send_command('ATZ', '%BOOTEV:0') # restart
     ensure_send_command('AT')
     ensure_send_command('AT%PDNSET=1,"DATA.MONO","IP"')
-    ensure_send_command('AT+COPS=0')
+    # ensure_send_command('AT+COPS=0')
     ensure_send_command('AT+CFUN=1')
-    ensure_send_command('AT%SETACFG="manager.urcBootEv.enabled","true"')
+    ensure_send_command('AT%SETACFG="manager.urcBootEv.enabled","true"',)
     ensure_send_command('AT%SETACFG="radiom.config.multi_rat_enable","true"')
     ensure_send_command('AT%SETACFG="radiom.config.preferred_rat_list","none"')
     ensure_send_command('AT%SETACFG="radiom.config.auto_preference_mode","none"')
     ensure_send_command('AT%SETACFG="locsrv.operation.locsrv_enable","true"')
     ensure_send_command('AT%SETACFG="locsrv.internal_gnss.auto_restart","enable"')
     ensure_send_command('AT%SETACFG="modem_apps.Mode.AutoConnectMode","true"')
-    ensure_send_command('AT%RATACT="NBNTN","1"') # if I want satellite
-    # ensure_send_command('AT%RATACT="CATM","1"') # if I want AT&T
+    # ensure_send_command('AT%RATACT="NBNTN","1"') # if I want satellite
+    ensure_send_command('AT%RATACT="CATM","1"') # if I want AT&T
     ensure_send_command('AT+CFUN=0') # stop radio
     ensure_send_command('AT%PDNSET=1,"DATA.MONO","IP"')
     ensure_send_command('AT%SETSYSCFG=SW_CFG.nb_band_table.band#1,ENABLE;23')
     ensure_send_command('AT%NTNCFG="POS","IGNSS","0"')
-    ensure_send_command('ATZ') # restart
+    ensure_send_command('ATZ', '%BOOTEV:0') # restart
     ensure_send_command('AT+CFUN=0') # only need to do again if I restart the module
     ensure_send_command('AT%IGNSSEV="FIX",1') # GPS coordinates subscription
-    ensure_send_command('AT%NOTIFYEV="SIB31",1') # satellite subscription
+    # ensure_send_command('AT%NOTIFYEV="SIB31",10') # satellite subscription
     ensure_send_command('AT+CEREG=2')
     ensure_send_command('AT%IGNSSACT=1')
-    ensure_send_command('AT+CFUN=1') # reenable radio
+    ensure_send_command('AT+CFUN=1', '+CEREG: 5') # reenable radio
     ensure_send_command('AT+COPS?')
 
-def ensure_send_command(command):
-    while(True):
+def ensure_send_command(command, confirm = "OK"):
+    ret = ""
+    while(len(ret) == 0 or ret[0] != command):
         ret = send_command(command)
-        try:
-            if(ret[-1] == "OK"):
-                break
-        except IndexError:
-            pass
+    while(len(ret)==0 or ret[-1][:len(confirm)] != confirm):
+        ret = read_response()
 
 def send_command(command):
     global ser, sio
